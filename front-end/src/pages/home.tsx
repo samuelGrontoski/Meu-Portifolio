12896e556fd898a3f371ca03b19bfcf35a5f6a0e
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2024-05-17 09:13:11 -0300

    att

--- front-end/src/pages/home.tsx
+++ front-end/src/pages/home.tsx
@@ -3,6 +3,7 @@ import { SocialButton } from "../components/socialButton";
 
 // Imagens
 import logo from "../assets/logo.svg"
+import pikachu_running from "../assets/pikachu-running.gif"
 import top from "../assets/top.svg"
 import botton from "../assets/botton.svg"
 import { AiOutlineMail, AiFillGithub, AiFillLinkedin, AiOutlineInstagram } from "react-icons/ai";
@@ -10,8 +11,11 @@ import { AiOutlineMail, AiFillGithub, AiFillLinkedin, AiOutlineInstagram } from 
 export function Home() {
     return (
         <div className="bg-bg w-screen h-screen">
-            <header>
-                <nav className="fixed top-12 right-24 flex justify-items-center content-start gap-6">
+            <header className="w-screen inline-flex justify-between p-5">
+                <div className="w-[64px] hover:scale-110 transition duration-200">
+                    <img src={logo} alt="logo" />
+                </div>
+                <nav className="flex justify-items-center content-start gap-6">
                     <SocialButton link="mailto:contatosamuelgrontoski@gmail.com" icon={AiOutlineMail} />
                     <SocialButton link="https://github.com/samuelGrontoski" icon={AiFillGithub} />
                     <SocialButton link="https://www.linkedin.com/in/samuel-grontoski/" icon={AiFillLinkedin} />
@@ -20,18 +24,17 @@ export function Home() {
             </header>
             <body>
                 <main>
-                    <div className="animate-pulse">
+                    {/* <div className="animate-pulse">
                         <img src={top} alt="barra superior" className="w-60 fixed left-12 top-0 rounded-lg"/>
                         <img src={botton} alt="barra inferior" className="w-60 fixed right-12 bottom-0 rounded-lg"/>
-                    </div>
+                    </div> */}
                     <button className="fixed left-20 top-8 w-[64px] hover:scale-110 transition duration-200">
-                        <div className="flex justify-start"> 
-                            <img src={logo} alt="logo"/>
-                        </div>
+
                     </button>
                 </main>
-                <footer className="text-cinza-300 fixed bottom-8 right-20 text-sm">
-                    ©2024 Samuel Grontoski
+                <footer className="inline-flex p-5 absolute inset-x-0 bottom-0">
+                    <img className="w-20" src={pikachu_running} alt="jolton_pokemon gif" />
+                    <p className="text-cinza-300 text-sm absolute right-5 bottom-5">©2024 Samuel Grontoski</p>
                 </footer>
             </body>
         </div>
