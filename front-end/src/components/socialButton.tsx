f029686aa2e93d8d89adeba37be97c655253642d
samuelGrontoski <contatosamuelgrontoski@gmail.com>
2024-06-11 12:20:53 -0300

    Menu hamburguer mobile

--- front-end/src/components/socialButton.tsx
+++ front-end/src/components/socialButton.tsx
@@ -1,7 +1,7 @@
-export function SocialButton ({ link, icon: Icon}) {
+export function SocialButton (props: any) {
     return (
-      <a href={link} className="grid place-content-center">
-        <Icon className="w-7 h-7 text-2xl text-cinza-500 hover:scale-125 hover:text-cinza-300 transition duration-200" />
+      <a href={props.link} className="grid place-content-center">
+        <props.icon className="w-7 h-7 text-cinza-500 hover:scale-125 hover:text-cinza-300 transition duration-200" />
       </a>
     );
   }
--- front-end/src/pages/home.tsx
+++ front-end/src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 // Components
 import { SocialButton } from "../components/socialButton";
 
@@ -5,35 +6,56 @@ import { SocialButton } from "../components/socialButton";
 import logo from "../assets/logo.svg"
 import pikachu_running from "../assets/pikachu-running.gif"
 import { AiOutlineMail, AiFillGithub, AiFillLinkedin, AiOutlineInstagram } from "react-icons/ai";
+import { IoMenu } from "react-icons/io5";
 
 export function Home() {
+
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
         <div className="bg-bg w-screen h-screen">
-            <head className="w-screen inline-flex justify-between p-5">
+            <div className="w-screen inline-flex justify-between p-5 md:px-40 md:pt-20">
                 <figure className="w-[64px] hover:scale-110 transition duration-200">
                     <img src={logo} alt="logo" />
                 </figure>
+                <nav>
+                    <button
+                        onClick={() => { console.log("Clicou no menu"), setShowMenu(!showMenu) }}
+                    >
+                        <IoMenu className="w-8 h-8 text-cinza-500 hover:text-cinza-300 transition duration-200 md:hidden" />
+                    </button>
+                </nav>
                 <nav className="justify-items-center content-start gap-6 hidden md:flex">
                     <SocialButton link="mailto:contatosamuelgrontoski@gmail.com" icon={AiOutlineMail} />
                     <SocialButton link="https://github.com/samuelGrontoski" icon={AiFillGithub} />
                     <SocialButton link="https://www.linkedin.com/in/samuel-grontoski/" icon={AiFillLinkedin} />
                     <SocialButton link="https://www.instagram.com/samuel_grontoski/" icon={AiOutlineInstagram} />
                 </nav>
-            </head>
-            <body>
-                <main>
-                    {/* <div className="animate-pulse">
-                        <img src={top} alt="barra superior" className="w-60 fixed left-12 top-0 rounded-lg"/>
-                        <img src={botton} alt="barra inferior" className="w-60 fixed right-12 bottom-0 rounded-lg"/>
-                    </div> */}
-                    <div className="fixed left-20 top-8 w-[64px] hoer:scale-110 transition duration-200" />
-
+            </div>
+            <div>
+                <main className="p-5 text-cinza-300 w-full md:px-40">
+                    <h1 className="text-lg font-bold text-center md:text-start md:text-2xl">Olá! Eu sou Samuel Grontoski</h1>
+                    <p className="text-base text-center my-5 md:text-start">Desenvolvedor Web Full Stack</p>
                 </main>
-                <footer className="flex justify-between items-end p-5 absolute inset-x-0 bottom-0 w-screen">
-                    <img className="w-20" src={pikachu_running} alt="jolton_pokemon gif" />
-                    <p className="text-cinza-300 text-sm">©2024 Samuel Grontoski</p>
+                <footer className="flex justify-between items-end p-5 absolute inset-x-0 bottom-0 w-screen md:px-40 md:mb-20">
+                    <img className="w-20" src={pikachu_running} alt="pikachu_gif" />
+                    <p className="text-cinza-300 text-sm">© 2024 Samuel Grontoski</p>
                 </footer>
-            </body>
+            </div>
+
+            {showMenu && (
+                <nav className="absolute right-0 top-0 p-5 flex flex-col gap-6 bg-bg md:hidden">
+                    <button
+                        onClick={() => { console.log("Clicou no menu"), setShowMenu(!showMenu) }}
+                    >
+                        <IoMenu className="w-8 h-8 text-cinza-500 hover:text-cinza-300 transition duration-200" />
+                    </button>
+                    <SocialButton link="mailto:contatosamuelgrontoski@gmail.com" icon={AiOutlineMail} />
+                    <SocialButton link="https://github.com/samuelGrontoski" icon={AiFillGithub} />
+                    <SocialButton link="https://www.linkedin.com/in/samuel-grontoski/" icon={AiFillLinkedin} />
+                    <SocialButton link="https://www.instagram.com/samuel_grontoski/" icon={AiOutlineInstagram} />
+                </nav>
+            )}
         </div>
     )
 }
